d733000610fc84f7a7b47b63d3e512a324fd4f06
Paul Cohen <paul.cohen@seibostudios.se>
2025-03-09 12:00:09 +0100

    Add flake8 config file and fixa number of flake8 warnings

--- adb-get.py
+++ adb-get.py
@@ -11,7 +11,7 @@ import dateutil.parser
 import requests
 import pprint
 import obsapi
-#from requests_oauthlib import OAuth2Session
+# from requests_oauthlib import OAuth2Session
 
 # Constants
 DEFAULT_CONF_FILE_PATH = 'adb-get.conf'
@@ -42,7 +42,8 @@ def species_api_key():
 
 
 def observations_sandbox_api_key():
-    """Value of the Observations SANDBOX API key environment variable if it exists, otherwise None."""
+    """Value of the Observations SANDBOX API key environment variable if it exists, otherwise
+    None."""
     if ADB_OBSERVATIONS_SANDBOX_API_KEY_ENV_NAME in os.environ:
         return os.environ[ADB_OBSERVATIONS_SANDBOX_API_KEY_ENV_NAME]
     else:
@@ -57,7 +58,7 @@ def observations_api_key():
         return None
 
 
-def print_http_response (r):
+def print_http_response(r):
     """Print the HTTP resonse (from a requests.get call) to stdout."""
     print('HTTP Status code: %s' % (r.status_code))
     print('HTTP Response headers:')
@@ -68,7 +69,8 @@ def print_http_response (r):
 
 
 def get_taxon_id_by_name(args, species_url, taxon_name):
-    """Artdatabanken's taxon id for the taxon with the given 'taxon_name'. Returns None if not found. """
+    """Artdatabanken's taxon id for the taxon with the given 'taxon_name'. Returns None if not
+       found."""
     url = '%s%s' % (species_url, 'speciesdata/search?searchString=%s' % (taxon_name))
     r = requests.get(url, headers=auth_headers(args.species_api_key))
     if args.verbose:
@@ -81,7 +83,8 @@ def get_taxon_id_by_name(args, species_url, taxon_name):
 
 
 def get_taxon_by_id(args, species_url, taxon_id):
-    """Artdatabanken's taxon data for the taxon with the given 'taxon_id'. Returns None if not found. """
+    """Artdatabanken's taxon data for the taxon with the given 'taxon_id'. Returns None if not
+       found."""
     url = '%s%s' % (species_url, 'speciesdata?taxa=%s' % (taxon_id))
     r = requests.get(url, headers=auth_headers(args.species_api_key))
     if args.verbose:
@@ -93,7 +96,7 @@ def get_taxon_by_id(args, species_url, taxon_id):
         return r.json()
 
 
-def get_observations (args, api_key, observations_url, taxon_id):
+def get_observations(args, api_key, observations_url, taxon_id):
     """Artdatabanken's observations for the taxon with the given 'taxon_id', Returns a dictionary
        with four keys; 'observations' which contains the list of observations retrieved in time
        order, 'total_count' which contains the total count of observationsi, and 'from_date' and
@@ -135,8 +138,10 @@ def pretty_print_observation(o):
     fdate = dateutil.parser.parse(o['startDate'])
     edate = dateutil.parser.parse(o['endDate'])
     if fdate != edate:
-        if fdate.hour != 0 and fdate.minute != 0 and edate.hour !=0 and edate.minute != 0:
-            print("%s %s-%s" % ('{:%Y-%m-%d}'.format(fdate), '{:%H:%M}'.format(fdate), '{:%H:%M}'.format(edate)))
+        if fdate.hour != 0 and fdate.minute != 0 and edate.hour != 0 and edate.minute != 0:
+            print("%s %s-%s" % ('{:%Y-%m-%d}'.format(fdate),
+                                '{:%H:%M}'.format(fdate),
+                                '{:%H:%M}'.format(edate)))
         else:
             print("%s" % ('{:%Y-%m-%d}'.format(fdate)))
     else:
@@ -164,23 +169,29 @@ def pretty_print_observation(o):
     osm_url = "https://www.openstreetmap.org/?mlat=%s&mlon=%s" % (northing, easting)
     print(" Google Maps location: %s" % (gm_url))
     print(" Open Street Maps location: %s" % (osm_url))
-#    print(" Google Maps location: %s" % ("https://www.google.com/maps/@?api=1&map_action=map&center=56.711061,16.3535513&zoom=12&basemap=terrain"))
-#    print(" Open Street Maps location: %s" % ("https://www.openstreetmap.org/?mlat=-38.3653&mlon=144.9069#map=9/-38.3653/144.9069"))
+#    print(" Google Maps location: %s" % ("https://www.google.com/maps/@?api=1&
+# map_action=map&center=56.711061,16.3535513&zoom=12&basemap=terrain"))
+#    print(" Open Street Maps location: %s" % ("https://www.openstreetmap.org/?
+# mlat=-38.3653&mlon=144.9069#map=9/-38.3653/144.9069"))
 
 
-def ping_apis (species_api_key, observations_api_key, observations_sandbox_api_key, verbose=False):
+def ping_apis(species_api_key, observations_api_key, observations_sandbox_api_key, verbose=False):
     """Ping the API:s at some root URL and check that we get a HTTP STATUS code 200."""
     species_url = '%s%s' % (ADB_API_ROOT_URL, ADB_SPECIES_API_PATH)
     observations_url = '%s%s' % (ADB_API_ROOT_URL, ADB_OBSERVATIONS_API_PATH)
     observations_sandbox_url = '%s%s' % (ADB_API_ROOT_URL, ADB_OBSERVATIONS_SANDBOX_API_PATH)
     ping_url = '%s%s' % (species_url, 'speciesdata/search?searchString=Tajgasångare')
     r = requests.get(ping_url, headers=auth_headers(species_api_key))
-    print('Artdatabanken Species API. URL: https://api-portal.artdatabanken.se/Products/species-information')
+    s = ("Artdatabanken Species API. "
+         "URL: https://api-portal.artdatabanken.se/Products/species-information")
+    print(s)
     print('GET %s' % ping_url)
     print('HTTP Status code: %s' % (r.status_code))
     ping_url = '%s%s' % (observations_sandbox_url, 'environment')
     r = requests.get(ping_url, headers=auth_headers(observations_sandbox_api_key))
-    print('Artdatabanken Observations SANDBOX API. URL: https://api-portal.artdatabanken.se/Products/sandbox-species-read')
+    s = ("Artdatabanken Observations SANDBOX API. "
+         "URL: https://api-portal.artdatabanken.se/Products/sandbox-species-read")
+    print(s)
     print('GET %s' % ping_url)
     print('HTTP Status code: %s' % (r.status_code))
     api = obsapi.AdbObsAPI(observations_api_key)
@@ -197,21 +208,22 @@ def today_RFC3339():
 
 
 def main():
-    parser = argparse.ArgumentParser(description='''CLI-program for getting stuff from the Artdatabanken API:s.''')
+    desc = "CLI-program for getting stuff from the Artdatabanken API:s."
+    parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('-v', '--verbose', action='store_true', default=False,
                         help="print info about what's going on [False].")
     parser.add_argument('-c', '--conf-file-path', default=DEFAULT_CONF_FILE_PATH,
                         help="Configuration file path [%s]." % (DEFAULT_CONF_FILE_PATH))
     parser.add_argument('-p', '--ping-the-apis', action='store_true', default=False,
-                        help="Ping the Artportalen API:s to check authentication and that it is alive.")
+                        help="Ping API:s to check authentication and that they are alive.")
     parser.add_argument('--species-api-key', default=species_api_key(),
-                        help="Species API authentication (subscription) key [value of environment variable %s]" %
+                        help="Species API authentication (subscription) key [%s]" %
                         (ADB_SPECIES_API_KEY_ENV_NAME))
     parser.add_argument('--observations-api-key', default=observations_api_key(),
-                        help="Observations API authentication (subscription) key [value of environment variable %s]" %
+                        help="Observations API authentication (subscription) key [%s]" %
                         (ADB_OBSERVATIONS_API_KEY_ENV_NAME))
     parser.add_argument('--observations-sandbox-api-key', default=observations_sandbox_api_key(),
-                        help="Observations SANDBOX API authentication (subscription) key [value of environment variable %s]" %
+                        help="Observations SANDBOX API authentication (subscription) key [%s]" %
                         (ADB_OBSERVATIONS_SANDBOX_API_KEY_ENV_NAME))
     parser.add_argument('-P', '--use-production-api', action='store_true', default=False,
                         help="Use the production (and not the sandbox)) API [False]")
@@ -242,7 +254,10 @@ def main():
         print("Error: No Observations API key provided (--observations-api-key)")
         sys.exit(2)
     if args.ping_the_apis:
-        ping_apis(args.species_api_key, args.observations_api_key, args.observations_sandbox_api_key, args.verbose)
+        ping_apis(args.species_api_key,
+                  args.observations_api_key,
+                  args.observations_sandbox_api_key,
+                  args.verbose)
         sys.exit(0)
     if args.get_observations:
         result = None
@@ -253,7 +268,9 @@ def main():
             else:
                 taxon_id = get_taxon_id_by_name(args, species_url, args.taxon_name)
                 if not taxon_id:
-                    print("Error: No taxon with name '%s' found in Artdatabankens Species API." % (args.taxon_name))
+                    errmsg = (f"Error: No taxon with name '{args.taxon_name}' found "
+                              "in Artdatabankens Species API.")
+                    print(errmsg)
                     sys.exit(3)
         else:
             taxon_id = args.taxon_id
@@ -263,7 +280,9 @@ def main():
             api_key = observations_sandbox_api_key()
             result = get_observations(args, api_key, observations_url, taxon_id)
         else:
-            print("Info: Using observations API. NOTE: This currently just does a predefined search and ignores the CLI arguments!")
+            infomsg = ("Info: Using observations API. NOTE: This currently just does a predefined "
+                       "search and ignores the CLI arguments!")
+            print(infomsg)
             api_key = observations_api_key()
             api = obsapi.AdbObsAPI(api_key)
             r = api.observations(obsapi.observations_search_filter())
@@ -277,11 +296,11 @@ def main():
             fdate = dateutil.parser.parse(result['from_date'])
             tdate = dateutil.parser.parse(result['to_date'])
             print("%s" % (50*'-'))
-            print("%d observations (of total %s) shown of %s between %s and %s" % (len(result['observations']),
-                                                                                   result['total_count'],
-                                                                                   args.taxon_name,
-                                                                                   '{:%Y-%m-%d}'.format(fdate),
-                                                                                   '{:%Y-%m-%d}'.format(tdate)))
+            fd = "{:%Y-%m-%d}".format(fdate)
+            td = "{:%Y-%m-%d}".format(tdate)
+            s = (f"{len(result['observations'])} observations of total {result['total_count']} "
+                 f"shown of {args.taxon_name} between {fd} and {td}")
+            print(s)
         else:
             if result:
                 for observation in result['observations']:
@@ -295,17 +314,21 @@ def main():
             else:
                 pprint.pprint(t)
         else:
-            print("Error: No taxon with id '%s' found in Artdatabanken's Species API" % (args.taxon_id))
+            errmsg = (f"Error: No taxon with id '{args.taxon_id}' found in Artdatabanken's Species "
+                      "API")
+            print(errmsg)
             sys.exit(3)
         sys.exit(0)
     if args.taxon_name:
         taxon_id = get_taxon_id_by_name(args, species_url, args.taxon_name)
         if not taxon_id:
-            print("Error: No taxon with name '%s' found in Artdatabanken's Species API." % (args.taxon_name))
+            errmsg = (f"Error: No taxon with name '{args.taxon_name}' found in Artdatabanken's "
+                      "Species API.")
+            print(errmsg)
             sys.exit(4)
         t = get_taxon_by_id(args, species_url, taxon_id)
         if args.pretty_print:
-            pretty_print_taxon (t[0])
+            pretty_print_taxon(t[0])
         else:
             pprint.pprint(t)
         sys.exit(0)
--- obsapi.py
+++ obsapi.py
@@ -11,17 +11,18 @@
 import requests
 import json
 import pprint
+import os
 
 # Constants
 API_NAME = 'Artdatabankens Species Observation System API'
 API_INFO_URL = 'https://api-portal.artdatabanken.se/Products/sos'
 API_ROOT_URL = 'https://api.artdatabanken.se'
 API_ROOT_PATH = '/species-observation-system/v1/'
-API_PING_RESOURCE='environment'
+API_PING_RESOURCE = 'environment'
 ADB_COORDINATSYSTEM_WGS_84_ID = 10
 
 
-def auth_headers (api_key, auth_token=None):
+def auth_headers(api_key, auth_token=None):
     """Returns a dictionary of authentication headers."""
     h = {'Ocp-Apim-Subscription-Key': api_key}
     if auth_token:
@@ -29,7 +30,7 @@ def auth_headers (api_key, auth_token=None):
     return h
 
 
-def species_api_key ():
+def species_api_key():
     """Value of the Species API key environment variable if it exists, otherwise None."""
     if ADB_SPECIES_API_KEY_ENV_NAME in os.environ:
         return os.environ[ADB_SPECIES_API_KEY_ENV_NAME]
@@ -37,7 +38,7 @@ def species_api_key ():
         return None
 
 
-def print_http_response (r):
+def print_http_response(r):
     """Print the HTTP resonse (from a requests.get call) to stdout."""
     print('HTTP Status code: %s' % (r.status_code))
     print('HTTP Response headers:')
@@ -47,17 +48,17 @@ def print_http_response (r):
         pprint.pprint(r.json())
 
 
-def api_info_url ():
+def api_info_url():
     """URL to info page about API."""
     return "https://api-portal.artdatabanken.se/Products/sos"
 
 
-def ping_url ():
+def ping_url():
     """Returns the URL used to ping the API."""
     return "%s%s%s" % (API_ROOT_URL, API_ROOT_PATH, API_PING_RESOURCE)
 
 
-def observations_search_filter ():
+def observations_search_filter():
     """A search filter for observations."""
     s = """{
             "dataProvider": {"ids": [1]},
@@ -79,11 +80,11 @@ def observations_search_filter ():
 class AdbObsAPI():
     """Represents the API."""
 
-    def __init__ (self, api_key):
+    def __init__(self, api_key):
         """Create a new API instance. A valid API-key 'api_key' must be provided."""
         self.api_key = api_key
 
-    def ping (self, verbose=False):
+    def ping(self, verbose=False):
         """Call the root resource of the API. Returns a requests response object."""
         r = requests.get(ping_url(), headers=auth_headers(self.api_key))
         if verbose:
@@ -91,13 +92,13 @@ class AdbObsAPI():
             print_http_response(url, r)
         return r
 
-    def area_types (self):
+    def area_types(self):
         """Valid area types as documented here:
            https://github.com/biodiversitydata-se/SOS/blob/master/Docs/Vocabularies.md#areatype
-           in the form of a dictionary indexed by area type id, where the entries are Swedish (sv-SE)
-           and English (en-GB) names of the area types. Use the area id type when calling the API.
-           By calling areas with type codes 1...1000 you get slightly different area types than
-           documented above. The returned dictionary reflects all those area types returned by
+           in the form of a dictionary indexed by area type id, where the entries are Swedish
+           (sv-SE) and English (en-GB) names of the area types. Use the area id type when calling
+           the API. By calling areas with type codes 1...1000 you get slightly different area types
+           than documented above. The returned dictionary reflects all those area types returned by
            that API call."""
         return {1: {"sv-SE": "Kommun",
                     "en-GB": "Kommun"},
@@ -132,7 +133,7 @@ class AdbObsAPI():
                 30: {"sv-SE": "Atlas10x10",
                      "en-GB": "Atlas10x10"}}
 
-    def areas (self, types=None, search_string=None, index=0, count=10, verbose=False):
+    def areas(self, types=None, search_string=None, index=0, count=10, verbose=False):
         """Areas (regions)."""
         assert index >= 0
         assert count <= 1000
@@ -147,11 +148,14 @@ class AdbObsAPI():
             print_http_response(url, r)
         return r
 
-    def observations (self, search_filter, index=0, count=10, sort_by=None, sort_order="Desc", lang="sv-SE", sensitive=False, verbose=False):
+    def observations(self, search_filter, index=0, count=10, sort_by=None,
+                     sort_order="Desc", lang="sv-SE", sensitive=False, verbose=False):
         """Observations matching 'search_filter'. Yeah, butt ugly."""
         assert index >= 0
         assert count <= 1000
-        url = "%s%s%s?skip=%d&take=%d&" % (API_ROOT_URL, API_ROOT_PATH, "Observations/Search", index, count)
+        url = f"{API_ROOT_URL}{API_ROOT_PATH}Observations/search?skip={index}&take={count}&"
+# url = "%s%s%s?skip=%d&take=%d&" % (API_ROOT_URL, API_ROOT_PATH,
+# "Observations/Search", index, count)
         if sort_by:
             url = url + "sortBy=%s&" % (sort_by)
         url = url + "sortOrder=%s&translationCultureCode=%s&" % (sort_order, lang)
@@ -159,12 +163,16 @@ class AdbObsAPI():
             url = url + "sensitiveObservations=true"
         else:
             url = url + "sensitiveObservations=false"
-        r = requests.post(url, data=search_filter, headers={**auth_headers(self.api_key), **{"Content-Type": "application/json"}})
+        headers = {**auth_headers(self.api_key), **{"Content-Type": "application/json"}}
+        r = requests.post(url,
+                          data=search_filter,
+                          headers=headers)
         if verbose:
             print_http_response(url, r)
         return r
 
-    def observation (self, observation_id, output_field_set="All properties", lang="sv-SE", sensitive=False, verbose=False):
+    def observation(self, observation_id, output_field_set="All properties", lang="sv-SE",
+                    sensitive=False, verbose=False):
         """Observation with the given 'observation_id'."""
         url = "%s%s%s%d?" % (API_ROOT_URL, API_ROOT_PATH, "Observations/", observation_id, )
         url = url + "outputFieldSet=%s&translationCultureCode=%si&" % (output_field_set, lang)
@@ -176,4 +184,3 @@ class AdbObsAPI():
         if verbose:
             print_http_response(url, r)
         return r
-
